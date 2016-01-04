8ce1e9b84eb776c132b96f9698d98093e0dccee5
Matias Grioni <matgrioni@gmail.com>
2016-01-04 15:30:34 -0500

    Created data computional functions

--- app/src/main/java/utils/DataUtils.java
+++ app/src/main/java/utils/DataUtils.java
@@ -5,8 +5,16 @@ import java.util.Collection;
 /**
  * @author Matias Grioni
  * @created 1/1/16
+ *
+ * Data computational utility functions.
  */
 public class DataUtils {
+    /**
+     * Sums all terms in the data set.
+     *
+     * @param data The data set.
+     * @return The sum of the terms in the data set.
+     */
     public static int sum(Collection<Integer> data) {
         int sum = 0;
 
@@ -15,5 +23,34 @@ public class DataUtils {
         }
 
         return sum;
+    }
+
+    /**
+     * Computes the average for the given data set.
+     *
+     * @param data The data set.
+     * @return The average of the data set.
+     */
+    public static double average(Collection<Integer> data) {
+        return (double) sum(data) / data.size();
+    }
+
+    /**
+     * Computes the standard deviation for the given data set.
+     *
+     * @param data The data set.
+     * @return The standard deviation of the data set.
+     */
+    public static double stdev(Collection<Integer> data) {
+        double average = average(data);
+
+        double sqsum = 0;
+        for(int datum : data) {
+            double diff = datum - average;
+
+            sqsum += diff * diff;
+        }
+
+        return Math.sqrt(sqsum / data.size());
     }
 }
